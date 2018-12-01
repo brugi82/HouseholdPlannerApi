9afe362ece2c50ea11fc409a2b04bffc53945e0b
Milos Petrovic <milos.brugi@gmail.com>
2018-12-01 00:10:59 -0500

    Implementing registration

---
+++ Source/HouseholdPlannerApi/Controllers/AccountsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseholdPlannerApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AccountsController:ControllerBase
+    {
+        public AccountsController()
+        {
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterUser([FromBody] RegistrationModel registrationModel)
+        {
+            return Ok("Reg");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ConfirmEmail()
+        {
+            return Ok("Conf");
+        }
+    }
+}
