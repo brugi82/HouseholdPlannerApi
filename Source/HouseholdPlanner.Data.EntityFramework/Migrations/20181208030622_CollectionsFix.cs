b2697ab35632c6233dfeec25727460d387291d5a
Milos Petrovic <milos.brugi@gmail.com>
2018-12-07 22:20:01 -0500

    Added new migrations and instructions.

---
+++ Source/HouseholdPlanner.Data.EntityFramework/Migrations/20181208030622_CollectionsFix.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HouseholdPlanner.Data.EntityFramework.Migrations
+{
+    public partial class CollectionsFix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
